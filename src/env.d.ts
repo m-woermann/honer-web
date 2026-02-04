9ef83da981a6578a162aae08da6a49b7dff6e71e
m-woermann <michael.p.woermann@gmail.com>
2026-02-04 15:01:57 +0100

    Deploy readiness: Add .nojekyll, update gitignore, and save latest viewer changes

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,12 @@
 /// <reference types="astro/client" />
+
+interface Window {
+  speakerConfigsMap: any;
+  serverSpeakerConfigs: any[];
+  imageData: string[];
+  speakerData: {
+    positioningData: any;
+    speakerConfigs: any;
+    images?: string[];
+  };
+}
