c3329985d0d801cb6c5af6d27508b51592719d96
m-woermann <michael.p.woermann@gmail.com>
2026-02-04 15:50:11 +0100

    Configure for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  // REPLACE WITH YOUR SITE URL:
+  site: 'https://example.github.io',
+  // REPLACE WITH YOUR REPO NAME (e.g. '/my-repo') or remove if using custom domain:
+  base: '/repo-name',
+  
   integrations: [tailwind()],
-  // Enable client-side scripts
-  output: 'hybrid',
   vite: {
     // Configure Vite for handling Three.js imports
     ssr: {
